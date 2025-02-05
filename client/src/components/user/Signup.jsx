c382e476d68757e6fd3d947595d6165f3efa3f68
itsvikash93 <vikashkumar827070@gmail.com>
2025-02-05 13:46:25 +0530

    api uri changed

--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -17,7 +17,7 @@ const Signup = () => {
   };
   const handleGoogleSignup = () => {
     try {
-      window.location.href = "http://localhost:3000/api/auth/google";
+      window.location.href = "https://collabify-h1gc.onrender.com/api/auth/google";
     } catch (err) {
       toast.error(err.response.data.message);
     }
--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://collabify-h1gc.onrender.com/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://collabify-h1gc.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
 });
 
 export default instance;
