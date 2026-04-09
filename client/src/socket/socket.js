318514e82ca63e8115aa415bcfe0f4da672dc9ab
itsvikash93 <vikashkumar827070@gmail.comm>
2026-04-09 21:39:06 +0530

    socket uri updated

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 // src/socket/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(import.meta.env.VITE_API_URI, {
   withCredentials: true,
   autoConnect: false, // important
 });
