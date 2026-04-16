cad11ae75fe52b8c719c757127e534a0adc197ff
itsvikash93 <vikashkumar827070@gmail.comm>
2026-04-16 11:22:12 +0530

    vercel.json added

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,10 @@
 // src/socket/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_API_URI, {
+// Replace /api with empty string to connect to root namespace for sockets
+const SOCKET_URI = import.meta.env.VITE_API_URI.replace("/api", "");
+
+export const socket = io(SOCKET_URI, {
   withCredentials: true,
   autoConnect: false, // important
 });
